a917609a402d6ea7a321e12228f5070ab472c2d3
back__kom <alihoupeng@163.com>
2016-01-25 20:41:17 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- 网页脚 -->
     <div class="footDiv">
-      网站简介 - 广告服务 - 网站地图 - 帮助信息 - 联系方式<br>
+      网站简介 - 广告服 - 网站地图 - 帮助信息 - 联系方式<br>
       Copyrights &copy; 2014 hys.com All Rights Reserved
     </div>
     //abc
